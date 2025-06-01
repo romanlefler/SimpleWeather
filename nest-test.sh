7f06639b5cb15561388003e1fea180144084bf8b
Roman Lefler <romanlefler@proton.me>
2025-06-01 01:32:16 -0500

    Remove everything in test script to get rid of weird issues

--- nest-test.sh
+++ nest-test.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+UUID=$(awk -F'"' '/uuid/ { print $4 }' ./info/metadata.json)
+rm -rf $HOME/.local/share/gnome-shell/extensions/$UUID
+
+make clean
 make install
 
+export G_MESSAGES_DEBUG=all
 export MUTTER_DEBUG_DUMMY_MODE_SPECS=1280x1024
 export SHELL_DEBUG=all
 dbus-run-session -- gnome-shell --nested --wayland
