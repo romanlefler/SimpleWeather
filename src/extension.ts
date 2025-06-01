4c04a11599bd45c581cabcb9709e28c4c2683704
Roman Lefler <romanlefler@proton.me>
2025-06-01 01:34:11 -0500

    Show temperature on panel in a loop, refreshing every 15 min

--- src/extension.ts
+++ src/extension.ts
@@ -17,10 +17,15 @@
 
 import Clutter from "gi://Clutter";
 import Gio from "gi://Gio";
+import GLib from "gi://GLib";
 import St from 'gi://St';
 import { Extension } from "resource:///org/gnome/shell/extensions/extension.js";
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import { Provider } from "./providers/provider.js";
+import { OpenMeteo } from "./providers/openmeteo.js";
+import { LibSoup } from "./libsoup.js";
+import { TempUnits } from "./units.js";
 
 export default class SimpleWeatherExtension extends Extension {
 
@@ -28,26 +33,62 @@ export default class SimpleWeatherExtension extends Extension {
     #indicator? : PanelMenu.Button;
     #panelLabel? : St.Label;
 
+    #libsoup? : LibSoup;
+    #provider? : Provider;
+    #fetchLoopId? : number;
+
     enable() {
         this.#gsettings = this.getSettings();
+        this.#libsoup = new LibSoup();
+        this.#provider = new OpenMeteo(this.#libsoup);
 
         this.#indicator = new PanelMenu.Button(0.0, "Weather", false);
 
         this.#panelLabel = new St.Label({
-            text: "Test",
-            "y_align": Clutter.ActorAlign.CENTER,
+            text: "...",
+            y_align: Clutter.ActorAlign.CENTER,
             y_expand: true
         });
         this.#indicator.add_child(this.#panelLabel);
 
         Main.panel.addToStatusArea(this.uuid, this.#indicator);
+
+        this.#fetchLoopId = GLib.timeout_add_seconds(
+            GLib.PRIORITY_DEFAULT,
+            15 * 60,
+            this.#updateWeather
+        );
+        this.#updateWeather();
     }
 
     disable() {
-        this.#gsettings = undefined;
+        if(this.#fetchLoopId) {
+            GLib.source_remove(this.#fetchLoopId);
+            this.#fetchLoopId = undefined;
+        }
 
+        this.#gsettings = undefined;
         this.#indicator?.destroy();
         this.#indicator = undefined;
+        this.#libsoup?.free();
+        this.#libsoup = undefined;
+        this.#provider = undefined;
+    }
+
+    #updateWeather() {
+        this.#updateWeatherAsync().catch(err => {
+            console.error(err);
+        });
+        return GLib.SOURCE_CONTINUE;
+    }
+
+    async #updateWeatherAsync() {
+
+        if(!this.#provider) throw new Error("Provider was undefined!");
+        const w = await this.#provider!.fetchWeather();
+
+        this.#panelLabel!.text = `${Math.round(w.temp.get(TempUnits.Fahrenheit))}\u00B0`;
+
     }
 
 }
